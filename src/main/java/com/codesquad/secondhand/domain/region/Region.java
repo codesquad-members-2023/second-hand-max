ad30319a48977243e1913007eaac3ed6060e43a1
yeonise <cerealnum@gmail.com>
2023-08-23 20:21:58 +0900

    test #2: RegionRepositoryTest 작성
    불필요한 테스트를 삭제했습니다.
    - SecondHandApplicationTests.java
    
    추가한 테스트는 다음과 같습니다.
    - Pageable 객체를 받아서 해당 page의 동네 목록을 size개씩 반환한다.
    - 다음 데이터가 있다면 true, 없다면 false를 반환한다.
    
    테스트 환경 통합을 위한 클래스를 생성했습니다.
    - IntegrationTestSupport.java

--- src/main/java/com/codesquad/secondhand/domain/region/Region.java
+++ src/main/java/com/codesquad/secondhand/domain/region/Region.java
@@ -5,8 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class Region {
---
+++ src/test/java/com/codesquad/secondhand/IntegrationTestSupport.java
@@ -0,0 +1,9 @@
+package com.codesquad.secondhand;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+}
