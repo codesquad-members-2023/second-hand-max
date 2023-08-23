dc85004b30a27bbc986297649f9dbe46aaf45d25
yeonise <cerealnum@gmail.com>
2023-08-23 15:42:33 +0900

    chore: 프로젝트 초기 설정
    - domain class에 @Entity, @Id, @GeneratedValue annotation을 설정합니다.

--- src/main/java/com/codesquad/secondhand/domain/category/Category.java
+++ src/main/java/com/codesquad/secondhand/domain/category/Category.java
@@ -1,10 +1,15 @@
 package com.codesquad.secondhand.domain.category;
 
-import lombok.RequiredArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-@RequiredArgsConstructor
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private String imageUrl;
--- src/main/java/com/codesquad/secondhand/domain/region/Region.java
+++ src/main/java/com/codesquad/secondhand/domain/region/Region.java
@@ -1,7 +1,15 @@
 package com.codesquad.secondhand.domain.region;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Region {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 
