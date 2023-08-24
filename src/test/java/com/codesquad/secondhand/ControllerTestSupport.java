0325abc40300e760f564bcf8737573b4c7208de8
O_jeegu <lumirite0@gmail.com>
2023-08-24 16:08:20 +0900

    feat #1: categoryController.class 추가

--- src/test/java/com/codesquad/secondhand/ControllerTestSupport.java
+++ src/test/java/com/codesquad/secondhand/ControllerTestSupport.java
@@ -5,12 +5,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.codesquad.secondhand.api.controller.category.CategoryController;
 import com.codesquad.secondhand.api.controller.region.RegionController;
+import com.codesquad.secondhand.api.service.category.CategoryService;
 import com.codesquad.secondhand.api.service.region.RegionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(controllers = {
-	RegionController.class
+	RegionController.class,
+	CategoryController.class
 })
 public abstract class ControllerTestSupport {
 
@@ -22,5 +25,8 @@ public abstract class ControllerTestSupport {
 
 	@MockBean
 	protected RegionService regionService;
+
+	@MockBean
+	protected CategoryService categoryService;
 
 }
