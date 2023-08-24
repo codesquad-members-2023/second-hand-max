e76c3bd822c2096644453e49409471d28886a67b
yeonise <cerealnum@gmail.com>
2023-08-24 15:58:09 +0900

    feat #4: 나의 동네 목록 응답을 위한 Service Layer 구현
    - 나의 동네 조회 성공을 알리는 ResponseMessage를 추가합니다.

---
+++ src/main/java/com/codesquad/secondhand/api/service/user_region/UserRegionService.java
@@ -0,0 +1,28 @@
+package com.codesquad.secondhand.api.service.user_region;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.codesquad.secondhand.api.service.user_region.response.UserRegionResponse;
+import com.codesquad.secondhand.domain.user_region.UserRegionRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserRegionService {
+
+	private final UserRegionRepository userRegionRepository;
+
+	@Transactional(readOnly = true)
+	public List<UserRegionResponse> listUserRegions(Long userId) {
+		return userRegionRepository.findByUserId(userId)
+			.stream()
+			.map(UserRegionResponse::from)
+			.collect(Collectors.toUnmodifiableList());
+	}
+
+}
