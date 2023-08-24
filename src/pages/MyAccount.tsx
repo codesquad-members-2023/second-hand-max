b43505d62d1d6654eca7dbf2afce4c86c3784d41
jsh3418 <albert5428@gmail.com>
2023-08-24 17:49:59 +0900

    feat: MyAccount 페이지 TopBar 구현

--- src/pages/MyAccount.tsx
+++ src/pages/MyAccount.tsx
@@ -1,12 +1,23 @@
-import TopBar from "@components/TopBar";
+import TopBar from '@components/TopBar';
+import { styled } from 'styled-components';
 
 const MyAccount: React.FC = () => {
   return (
     <>
       <TopBar aria-label="내 계정">
+        <Title>내 계정</Title>
       </TopBar>
     </>
   );
 };
+
+const Title = styled.h2`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+
+  ${({ theme: { fonts } }) => fonts.display.strong16}
+`;
 
 export default MyAccount;
