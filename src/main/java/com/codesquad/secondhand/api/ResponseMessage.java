df9f8298aeb586ef5e9f168bc46efd6ee58565c9
yeonise <cerealnum@gmail.com>
2023-08-24 11:15:26 +0900

    refactor #2: ResponseMessage 형식을 통일합니다.

--- src/main/java/com/codesquad/secondhand/api/ResponseMessage.java
+++ src/main/java/com/codesquad/secondhand/api/ResponseMessage.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 public enum ResponseMessage {
 
 	CATEGORY_FETCH_SUCCESS("카테고리 목록 조회를 성공하였습니다."),
-	REGION_FETCH_SUCCESS("모든 동네 목록 조회에 성공하였습니다.");
+	REGION_FETCH_SUCCESS("동네 목록 조회를 성공하였습니다.");
 
 	private final String message;
 
