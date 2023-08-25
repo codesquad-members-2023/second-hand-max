24ec5ce7eafcc3889c8d07ae0faedd52028a395f
yeonise <cerealnum@gmail.com>
2023-08-25 17:18:19 +0900

    feat #4: 사용자 동네 삭제를 위한 Presentation Layer 추가

--- src/main/java/com/codesquad/secondhand/api/controller/user_region/UserRegionController.java
+++ src/main/java/com/codesquad/secondhand/api/controller/user_region/UserRegionController.java
@@ -3,8 +3,11 @@ package com.codesquad.secondhand.api.controller.user_region;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codesquad.secondhand.api.ApiResponse;
@@ -26,6 +29,14 @@ public class UserRegionController {
 		final Long userId = 1L;
 		return ApiResponse.of(HttpStatus.OK, ResponseMessage.USER_REGION_FETCH_SUCCESS.getMessage(),
 			userRegionService.listUserRegions(userId));
+	}
+
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public ApiResponse<Void> deleteUserRegion(@PathVariable Long id) {
+		final Long userId = 1L;
+		userRegionService.deleteUserRegion(userId, id);
+		return ApiResponse.of(HttpStatus.NO_CONTENT, ResponseMessage.USER_REGION_DELETE_SUCCESS.getMessage(), null);
 	}
 
 }
