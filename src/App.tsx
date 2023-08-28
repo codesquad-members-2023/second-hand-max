ac60858fc5fcd3fec35e0342d8ff63ce4426d153
jsh3418 <albert5428@gmail.com>
2023-08-28 14:03:45 +0900

    chore: styles 디렉토리명 design으로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import GlobalStyle from '@styles/GlobalStyle';
-import { theme } from '@styles/designSystem';
+import GlobalStyle from '@design/GlobalStyle';
+import { theme } from '@design/designSystem';
 import AppRoutes from 'routes';
 import { ThemeProvider } from 'styled-components';
 
