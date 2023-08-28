d74a74ca8d9b5d69444103d5a78a6f7e32539ad6
jsh3418 <albert5428@gmail.com>
2023-08-28 14:05:31 +0900

    feat: Icon 컴포넌트 그룹화

---
+++ src/design/Icon.ts
@@ -0,0 +1,15 @@
+import { ReactComponent as Home } from '@assets/icons/home.svg';
+import { ReactComponent as News } from '@assets/icons/news.svg';
+import { ReactComponent as Heart } from '@assets/icons/heart.svg';
+import { ReactComponent as Message } from '@assets/icons/message.svg';
+import { ReactComponent as UserCircle } from '@assets/icons/user-circle.svg';
+
+const Icon = {
+  Home,
+  News,
+  Heart,
+  Message,
+  UserCircle,
+};
+
+export default Icon;
