1efe65d6c116bc802621087acf54475d780b4d4c
O_jeegu <lumirite0@gmail.com>
2023-08-30 02:07:19 +0900

    feat #3: entity 매핑 설정

--- src/main/java/com/codesquad/secondhand/domain/item/Item.java
+++ src/main/java/com/codesquad/secondhand/domain/item/Item.java
@@ -1,28 +1,68 @@
 package com.codesquad.secondhand.domain.item;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
+import org.springframework.data.annotation.LastModifiedDate;
+
+import com.codesquad.secondhand.domain.category.Category;
+import com.codesquad.secondhand.domain.region.Region;
+import com.codesquad.secondhand.domain.user.User;
+import com.codesquad.secondhand.util.BaseTimeEntity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
 @Entity
-public class Item {
+@Table(name = "user")
+public class Item extends BaseTimeEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private Long userId;
-	private Long categoryId;
-	private Long regionId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "region_id")
+	private Region region;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "status_id")
+	private ItemStatus itemStatus;
+
+	@OneToMany(mappedBy = "item", cascade = CascadeType.ALL)
+	private List<ItemImage> itemImages = new ArrayList<>();
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
+
 	private String title;
 	private String content;
-	private int price;
-	private ItemStatus status;
-	private int views;
-	private LocalDateTime postedAt;
-	private LocalDateTime updatedAt;
+	private Integer price;
 	private boolean isDeleted;
 
 }
