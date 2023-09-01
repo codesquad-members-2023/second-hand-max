52f4df88ab08787349f1ee78eed67c8a72e72ee4
O_jeegu <lumirite0@gmail.com>
2023-09-01 11:29:12 +0900

    feat #3: chat, wishlist entity table 임시 생성(추후 수정 필요)

---
+++ src/main/java/com/codesquad/secondhand/domain/chat/Chat.java
@@ -0,0 +1,35 @@
+package com.codesquad.secondhand.domain.chat;
+
+//todo : 임시 클래스. 필드 수정 필수
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.codesquad.secondhand.domain.item.Item;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "chat")
+public class Chat {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
+
+}
---
+++ src/main/java/com/codesquad/secondhand/domain/wishlist/WishList.java
@@ -0,0 +1,39 @@
+package com.codesquad.secondhand.domain.wishlist;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.codesquad.secondhand.domain.item.Item;
+import com.codesquad.secondhand.domain.user.User;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "wishlist")
+public class WishList {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
+
+}
