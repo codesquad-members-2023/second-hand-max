e57b9413329f62c2f2a97fc73fcfb5fbaaaff1ca
O_jeegu <lumirite0@gmail.com>
2023-09-01 11:29:43 +0900

    feat #3: wishlist 매핑 추가

--- src/main/java/com/codesquad/secondhand/domain/user/User.java
+++ src/main/java/com/codesquad/secondhand/domain/user/User.java
@@ -1,17 +1,21 @@
 package com.codesquad.secondhand.domain.user;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.codesquad.secondhand.domain.region.Region;
 import com.codesquad.secondhand.domain.user_region.UserRegion;
+import com.codesquad.secondhand.domain.wishlist.WishList;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -41,6 +45,9 @@ public class User {
 
 	@Embedded
 	private MyRegion myRegion;
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+	private List<WishList> wishLists = new ArrayList<>();
 
 	public List<UserRegion> listUserRegion() {
 		return myRegion.listAll();
