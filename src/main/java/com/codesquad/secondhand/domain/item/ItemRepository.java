8872330f367621f9543b7ef14f83e9dceecd2bed
O_jeegu <lumirite0@gmail.com>
2023-09-04 15:48:03 +0900

    refactor #3: 사용하지 않는 메서드 삭제

--- src/main/java/com/codesquad/secondhand/domain/item/ItemRepository.java
+++ src/main/java/com/codesquad/secondhand/domain/item/ItemRepository.java
@@ -1,23 +1,7 @@
 package com.codesquad.secondhand.domain.item;
 
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-
-	@Query(
-		"SELECT i FROM Item i "
-			+ "LEFT JOIN FETCH i.category c "
-			+ "LEFT JOIN FETCH i.region r "
-			+ "WHERE i.isDeleted = false "
-			+ "AND c.id = :categoryId "
-			+ "AND r.id = :regionId "
-			+ "ORDER BY i.updatedAt DESC"
-	)
-	Slice<Item> findSliceBy(@Param("categoryId") Long categoryId, @Param("regionId") Long regionId,
-		@Param("pageable") Pageable pageable);
 
 }
