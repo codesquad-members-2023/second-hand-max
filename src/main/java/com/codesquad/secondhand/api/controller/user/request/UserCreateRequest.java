8c6ef6748a2e63a56a82d343672cbec246737854
yeonise <cerealnum@gmail.com>
2023-09-04 20:02:57 +0900

    fix #27: 정규식 패턴 수정

--- src/main/java/com/codesquad/secondhand/api/controller/user/request/UserCreateRequest.java
+++ src/main/java/com/codesquad/secondhand/api/controller/user/request/UserCreateRequest.java
@@ -12,13 +12,13 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserCreateRequest {
 
-	@Pattern(regexp = "^(?=.*[a-zA-Z가-힣])[a-zA-Z가-힣0-9]*$", message = "닉네임은 2자 이상 10자 이하, 영문 또는 한글을 포함해야 합니다")
+	@Pattern(regexp = "^(?=.*[a-zA-Z가-힣])[a-zA-Z가-힣0-9]{2,10}$", message = "닉네임은 2자 이상 10자 이하, 영문 또는 한글을 포함해야 합니다")
 	private String nickname;
 
 	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "이메일 형식이 올바르지 않습니다")
 	private String email;
 
-	@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()-_+=])[A-Za-z\\d!@#$%^&*()-_+=]{8,16}$", message = "비밀번호는 8자 이상 16자 이하로, 영문, 숫자, 특수문자를 최소 1개씩 포함해야 합니다")
+	@Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]).{8,16}$", message = "비밀번호는 8자 이상 16자 이하로, 영문, 숫자, 특수문자를 최소 1개씩 포함해야 합니다")
 	private String password;
 
 	public UserCreateServiceRequest toService(Image image) {
