9c59e0b79decd4c938cbf3a5f811fb3a8bbbf11a
yeonise <cerealnum@gmail.com>
2023-09-04 20:02:35 +0900

    feat #27: BindException Handler 추가

--- src/main/java/com/codesquad/secondhand/exception/common/CommonExceptionHandler.java
+++ src/main/java/com/codesquad/secondhand/exception/common/CommonExceptionHandler.java
@@ -1,6 +1,12 @@
 package com.codesquad.secondhand.exception.common;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,6 +15,25 @@ import com.codesquad.secondhand.api.ApiResponse;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
+
+	private static final Logger logger = LoggerFactory.getLogger(CommonExceptionHandler.class);
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(BindException.class)
+	public ApiResponse<Object> handleBindException(BindException e) {
+		logger.debug("BindException handling : {}", e.toString());
+		return ApiResponse.of(
+			HttpStatus.BAD_REQUEST,
+			HttpStatus.BAD_REQUEST.getReasonPhrase(),
+			e.getBindingResult().getFieldErrors().stream().map(
+				error -> {
+					Map<String, String> errors = new HashMap<>();
+					errors.put("field", error.getField());
+					errors.put("defaultMessage", error.getDefaultMessage());
+					return errors;
+				})
+		);
+	}
 
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(InvalidCursorException.class)
