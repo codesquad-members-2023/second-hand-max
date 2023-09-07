d42ba43a4a996baf9064d815e592c7872e0ef07e
Seoyeon Kim <cerealnum@gmail.com>
2023-09-07 22:43:04 +0900

    allowedHeaders 설정 추가

--- src/main/java/com/codesquad/secondhand/config/WebConfig.java
+++ src/main/java/com/codesquad/secondhand/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedOrigins("*")
+			.allowedHeaders("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
 	}
 
