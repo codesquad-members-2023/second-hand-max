346d2e9598be57b4f3f902e45947c819cd6b3d81
O_jeegu <lumirite0@gmail.com>
2023-09-11 02:15:39 +0900

    feat #38: 상품이미지 객체에서 이미지 id를 가져오는 메서드 추가

--- src/main/java/com/codesquad/secondhand/domain/item_image/ItemImage.java
+++ src/main/java/com/codesquad/secondhand/domain/item_image/ItemImage.java
@@ -1,4 +1,4 @@
-package com.codesquad.secondhand.domain.item;
+package com.codesquad.secondhand.domain.item_image;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,10 +11,16 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.codesquad.secondhand.domain.image.Image;
+import com.codesquad.secondhand.domain.item.Item;
 import com.codesquad.secondhand.util.BaseTimeEntity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Getter
 @Entity
 @Table(name = "item_image")
@@ -32,4 +38,7 @@ public class ItemImage extends BaseTimeEntity {
 	@JoinColumn(name = "image_id")
 	private Image image;
 
+	public Long findImageId() {
+		return this.image.getId();
+	}
 }
