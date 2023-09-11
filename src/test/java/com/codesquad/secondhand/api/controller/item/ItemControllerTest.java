dae351a4dbd97a122a8f182d484ea98249257b8d
O_jeegu <lumirite0@gmail.com>
2023-09-11 16:08:40 +0900

    refactor #38: 불필요한 코드 삭제

--- src/test/java/com/codesquad/secondhand/api/controller/item/ItemControllerTest.java
+++ src/test/java/com/codesquad/secondhand/api/controller/item/ItemControllerTest.java
@@ -14,11 +14,8 @@ import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 
 import com.codesquad.secondhand.ControllerTestSupport;
-import com.codesquad.secondhand.annotation.SignInUser;
 import com.codesquad.secondhand.api.controller.item.request.ItemPostingRequest;
 import com.codesquad.secondhand.domain.image.Image;
-import com.codesquad.secondhand.domain.provider.Provider;
-import com.codesquad.secondhand.domain.user.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.Claims;
@@ -29,8 +26,6 @@ public class ItemControllerTest extends ControllerTestSupport {
 	@BeforeEach
 	void mockingJwtService() {
 		given(jwtService.parse(Mockito.any())).willReturn(createMockClaims());
-		Provider provider = new Provider(Provider.ofLocal().getId(), Provider.ofLocal().getType());
-		User user = new User(1L, null, null, provider, null, "name", "email@email.com", "password1!");
 	}
 
 	private Claims createMockClaims() {
@@ -43,7 +38,6 @@ public class ItemControllerTest extends ControllerTestSupport {
 	@Test
 	void postItem() throws Exception {
 		// given
-		SignInUser signInUser = new SignInUser(1L);
 		ItemPostingRequest request = new ItemPostingRequest("title", null, "content", List.of(1L, 2L), 1L, 1L);
 
 		// when
