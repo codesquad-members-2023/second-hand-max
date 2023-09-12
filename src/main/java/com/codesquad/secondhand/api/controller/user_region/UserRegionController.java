62a4c5e58b3d227537c077333ff7a1ee30da81f4
yeonise <cerealnum@gmail.com>
2023-09-12 17:14:50 +0900

    feat #50: 사용자 동네를 선택하는 컨트롤러 생성

--- src/main/java/com/codesquad/secondhand/api/controller/user_region/UserRegionController.java
+++ src/main/java/com/codesquad/secondhand/api/controller/user_region/UserRegionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,7 +44,12 @@ public class UserRegionController {
 		return ApiResponse.noData(HttpStatus.CREATED, ResponseMessage.USER_REGION_CREATE_SUCCESS.getMessage());
 	}
 
-	@ResponseStatus(HttpStatus.OK)
+	@PatchMapping("/{id}")
+	public ApiResponse<Void> selectUserRegion(@PathVariable Long id, @SignIn SignInUser signInUser) {
+		userRegionService.selectUserRegion(signInUser.getId(), id);
+		return ApiResponse.noData(HttpStatus.OK, ResponseMessage.USER_REGION_SELECT_SUCCESS.getMessage());
+	}
+
 	@DeleteMapping("/{id}")
 	public ApiResponse<Void> deleteUserRegion(@PathVariable Long id, @SignIn SignInUser signInUser) {
 		userRegionService.deleteUserRegion(signInUser.getId(), id);
