6cbd12a8d65974d7069e7da943b526eeaf2fa619
yeonise <cerealnum@gmail.com>
2023-09-12 18:02:51 +0900

    test #50: 사용자 동네 선택 API 관련 테스트 추가

--- src/test/java/com/codesquad/secondhand/domain/user/UserTest.java
+++ src/test/java/com/codesquad/secondhand/domain/user/UserTest.java
@@ -3,10 +3,13 @@ package com.codesquad.secondhand.domain.user;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Collection;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.codesquad.secondhand.FixtureFactory;
@@ -50,7 +53,7 @@ public class UserTest extends IntegrationTestSupport {
 
 	@DisplayName("사용자의 닉네임과 프로필을 수정한다.")
 	@Test
-	void updateNickname() {
+	void updateInformation() {
 		// given
 		List<Region> regions = FixtureFactory.createRegionFixtures(1);
 		regionRepository.saveAll(regions);
@@ -71,9 +74,43 @@ public class UserTest extends IntegrationTestSupport {
 		);
 	}
 
+	@DisplayName("사용자 동네 선택 시나리오")
+	@TestFactory
+	Collection<DynamicTest> updateSelectedRegion() {
+		// given
+		List<Region> regions = FixtureFactory.createRegionFixtures(2);
+		regionRepository.saveAll(regions);
+
+		User user = FixtureFactory.createUserFixture(List.of(regions.get(0)));
+		User savedUser = userRepository.save(user);
+
+		return List.of(
+			DynamicTest.dynamicTest("사용자 등록 시 기본 동네를 자동으로 선택한다.", () -> {
+				// when & then
+				assertThat(savedUser.getSelectedRegion()).isEqualTo(regions.get(0));
+			}),
+			DynamicTest.dynamicTest("사용자 동네에 없는 동네를 선택하려고 시도하는 경우 에러가 발생한다.", () -> {
+				// when & then
+				assertThatThrownBy(() -> savedUser.updateSelectedRegion(regions.get(1)))
+					.isInstanceOf(NoSuchUserRegionException.class)
+					.hasMessage("사용자 동네 목록에 없는 동네입니다");
+			}),
+			DynamicTest.dynamicTest("사용자 목록에 있는 동네를 선택할 수 있다.", () -> {
+				// given
+				user.addUserRegion(regions.get(1));
+
+				// when
+				savedUser.updateSelectedRegion(regions.get(1));
+
+				//then
+				assertThat(savedUser.getSelectedRegion()).isEqualTo(regions.get(1));
+			})
+		);
+	}
+
 	@DisplayName("사용자의 동네가 아닌 다른 동네에 새로운 상품을 등록하는 경우 예외가 발생한다.")
 	@Test
-	void  postItem() {
+	void postItem() {
 		//given
 		List<Region> regions = FixtureFactory.createRegionFixtures(3);
 		regionRepository.saveAll(regions);
