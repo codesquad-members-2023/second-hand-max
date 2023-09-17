3a7d73585a48eb6efbf4c0cf062e2883ec4dd777
jsh3418 <albert5428@gmail.com>
2023-09-17 12:59:09 +0900

    fix: ProductLIstItem import 경로 수정 (#22)

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -1,6 +1,6 @@
 import { styled } from 'styled-components';
 import ListItem from './ListItem';
-import { ProductListItem } from 'apis/types';
+import { ProductListItem } from 'types';
 
 const ProductList: React.FC = () => {
   return (
