46a45e8b4d01ec4fa806b8526fd1d16e67ddcd6d
jsh3418 <albert5428@gmail.com>
2023-09-17 13:37:30 +0900

    chore: ReactQueryDevtools 초기 열림 상태 수정 (#17)

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App: React.FC = () => {
         <GlobalStyle />
         <AppRoutes />
       </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 };
