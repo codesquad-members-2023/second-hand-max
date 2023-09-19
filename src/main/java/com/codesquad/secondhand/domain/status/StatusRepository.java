e2f353473aa730f0b28a050cfcc4958bd7fbf978
Ojeegu <lumirite0@gmail.com>
2023-09-19 13:55:25 +0900

    feat #78: 존재하지 않는 statusId를 받으면 예외를 발생하도록 검증 추가

--- src/main/java/com/codesquad/secondhand/domain/status/StatusRepository.java
+++ src/main/java/com/codesquad/secondhand/domain/status/StatusRepository.java
@@ -1,6 +1,11 @@
 package com.codesquad.secondhand.domain.status;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StatusRepository extends JpaRepository<Status, Long> {
+
+	boolean existsByIdIn(List<Long> statusIds);
+
 }
