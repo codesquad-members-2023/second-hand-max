52a2af7f04beabdc99e55588b64a7afb57a9c37b
jsh3418 <albert5428@gmail.com>
2023-09-19 14:29:52 +0900

    refactor: Category type 분리 (#22)

--- src/apis/category/types.ts
+++ src/apis/category/types.ts
@@ -1,11 +1,9 @@
+import { Category } from 'types/category';
+
 export type getCategoriesResponse = {
   statusCode: number;
   message: string;
   data: {
-    categories: {
-      id: number;
-      imageUrl: string;
-      name: string;
-    }[];
+    categories: Category[];
   };
 };
