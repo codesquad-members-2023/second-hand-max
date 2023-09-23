0f8e8c0a853752f08ddab917b0ae9eee1d541d8b
jsh3418 <albert5428@gmail.com>
2023-09-23 18:16:28 +0900

    feat: 상품 등록 성공시 등록 모달 꺼지도록 구현

--- src/hooks/queries/usePostProductMutation.ts
+++ src/hooks/queries/usePostProductMutation.ts
@@ -1,13 +1,18 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { postProduct } from 'apis/product';
+import { useModalStore } from 'stores/useModalStore';
 
 export const usePostProductMutation = () => {
   const queryClient = useQueryClient();
+  const closePostProductModal = useModalStore(
+    ({ closePostProductModal }) => closePostProductModal,
+  );
 
   return useMutation({
     mutationFn: postProduct,
     onSuccess() {
       alert('상품이 등록되었습니다.');
+      closePostProductModal();
       queryClient.invalidateQueries({
         queryKey: ['product'],
       });
