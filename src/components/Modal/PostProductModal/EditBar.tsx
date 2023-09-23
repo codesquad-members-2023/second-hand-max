44d59289994c54c34a1a144026a674180c5266f9
jsh3418 <albert5428@gmail.com>
2023-09-23 16:07:21 +0900

    chore: 상품 생성 페이지 지역 변경 기능 삭제

--- src/components/Modal/PostProductModal/EditBar.tsx
+++ src/components/Modal/PostProductModal/EditBar.tsx
@@ -1,17 +1,14 @@
 import BottomBar from '@components/BottomBar';
 import Icons from '@design/Icons';
 import styled from 'styled-components';
-import { useModalStore } from 'stores/useModalStore';
+
 import { useUserStore } from 'stores/useUserStore';
 
 export const EditBar: React.FC = () => {
   const currentRegion = useUserStore(({ currentRegion }) => currentRegion);
-  const openRegionSettingModal = useModalStore(
-    ({ openRegionSettingModal }) => openRegionSettingModal,
-  );
 
   return (
-    <StyleEditBar onClick={openRegionSettingModal}>
+    <StyleEditBar>
       <Icons.MapPinFilled />
       <div>{currentRegion.addressName}</div>
     </StyleEditBar>
@@ -21,5 +18,5 @@ export const EditBar: React.FC = () => {
 const StyleEditBar = styled(BottomBar)`
   padding: 16px;
   gap: 8px;
-  cursor: pointer;
+  user-select: none;
 `;
