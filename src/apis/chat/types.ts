7fec4c579d48b7d67efe4a0cb932cac8ff8ef430
jsh3418 <albert5428@gmail.com>
2023-10-04 12:38:30 +0900

    chore: API query, response 수정

--- src/apis/chat/types.ts
+++ src/apis/chat/types.ts
@@ -4,13 +4,11 @@ import { Chat } from 'types/chat';
 export type GetChatListParams = {
   page?: number;
   size?: number;
-  messageId?: string;
 };
 
 export type GetChatListResponse = {
   statusCode: number;
   data: {
-    lastmessageId: number;
     contents: Chat[];
     paging: Paging;
   };
